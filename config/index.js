544a8df50ea7b752265f8b37e1d2227c6eaf7f1f
gfguozhi <gfguozhi@gf.com.cn>
2019-11-25 18:53:39 +0800

    [bump] reus.js

--- config/index.js
+++ config/index.js
@@ -1,17 +1,1 @@
-module.exports = {
-  mongodb: [
-    {
-      alias: 'mongo1',
-      uri: 'mongodb://export.com:27017/dbname',
-      user: 'user',
-      password: 'pass',
-      poolSize: 2,
-    },
-  ],
-  sequelize: [
-    {
-      alias: 'seq1',
-      uri: 'postgres://user:pass@example.com:5432/dbname',
-    },
-  ],
-};
+module.exports = {};
