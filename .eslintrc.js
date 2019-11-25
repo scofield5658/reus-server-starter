51e3058a2fbdffe405972661c439f8bd7aa0d1e6
Guo <gggzzz@126.com>
2019-11-25 23:46:58 +0800

    [update] use airbnb

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  "root": true,
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true,
+    "jest": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "ecmaFeatures": {
+    "destructuring": true
+  },
+  "extends": "airbnb",
+  "rules": {
+    "indent": ["error", 2],
+    "linebreak-style": "off",
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "no-console": "off",
+    "no-debugger": "off",
+    "experimentalDecorators": "off",
+    "no-restricted-syntax": "off",
+    "no-restricted-globals": "off",
+    "global-require": "off",
+  },
+};
--- src/routers/hello.js
+++ src/routers/hello.js
@@ -1,14 +1,14 @@
 module.exports = [
   {
-    path: '/hello',
-    method: 'get',
-    controller: require('../controllers/hello'),
-    middlewares: [require('../middlewares/hello.mid')],
+    path: "/hello",
+    method: "get",
+    controller: require("../controllers/hello"),
+    middlewares: [require("../middlewares/hello.mid")],
   },
   {
-    path: '/redirect_sample',
-    method: 'get',
-    redirect: '/v3/weather/weatherInfo?key=162150bdfd5e3e12a2d882272232318b&city=%E5%B9%BF%E5%B7%9E&output=json',
-    target: 'http://restapi.amap.com',
+    path: "/redirect_sample",
+    method: "get",
+    redirect: "/v3/weather/weatherInfo?key=162150bdfd5e3e12a2d882272232318b&city=%E5%B9%BF%E5%B7%9E&output=json",
+    target: "http://restapi.amap.com",
   },
 ];
