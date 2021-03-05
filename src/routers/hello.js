8cfd514da53bec75fc8bad3e1fb23e09f9fc9228
scofield5658 <gggzzz@126.com>
2021-03-05 23:54:09 +0800

    [update] bump reus to 2.x && update demo of proxy

--- src/routers/hello.js
+++ src/routers/hello.js
@@ -8,7 +8,11 @@ module.exports = [
   {
     path: "/redirect_sample",
     method: "get",
-    redirect: "/v3/weather/weatherInfo?key=162150bdfd5e3e12a2d882272232318b&city=%E5%B9%BF%E5%B7%9E&output=json",
+    proxyPattern: "/redirect_sample",
+    pathRewrite: (path) => {
+      console.log(path);
+      return "/v3/weather/weatherInfo?key=162150bdfd5e3e12a2d882272232318b&city=%E5%B9%BF%E5%B7%9E&output=json"
+    },
     target: "http://restapi.amap.com",
   },
 ];
